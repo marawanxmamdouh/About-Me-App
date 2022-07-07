49c33536ed6adf5b54b6e81c1bbe98f1ddacfabb
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-07 21:41:43 +0200

    Add Btn Logic

--- app/src/main/java/dev/marawanxmamdouh/aboutme/MainActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/aboutme/MainActivity.kt
@@ -1,11 +1,21 @@
 package dev.marawanxmamdouh.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            tvNickname.text = etNickname.text
+            etNickname.visibility = View.GONE
+            btn.visibility = View.GONE
+            tvNickname.visibility = View.VISIBLE
+        }
     }
 }
